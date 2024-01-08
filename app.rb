247dd8ef1bb85a48ea6c31b7eb88b70ef733949b
SK2108 <85369151+SK2108@users.noreply.github.com>
2024-01-08 22:11:36 +0100

    Updating the code snippet for consuming RSS according to the standards in Ruby-doc 3.1.4

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'rss'
+require 'open-uri'
 
 url = ''
 open(url) do |rss|
-  feed = = RSS::Parser.parse(rss)
+  feed = RSS::Parser.parse(rss)
   puts "Title: #{feed.channel.title}"
   feed.items.each do |item|
     puts "Item: {item.title}"
